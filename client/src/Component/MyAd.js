baa222a3913b2967e58b51d31d444159ff56c4bb
imodclub <ikamol.me@gmail.com>
2024-09-02 19:26:38 +0700

    แก้ index.html

--- client/src/Component/MyAd.js
+++ client/src/Component/MyAd.js
@@ -3,7 +3,7 @@ import { Adsense } from '@ctrl/react-adsense';
 
 const MyAd = () => {
   return (
-    <Adsense.google
+    <Adsense
       client='ca-pub-3145740054914889' // แทนที่ด้วย ID ของคุณ
       slot='5913453722' // แทนที่ด้วย Slot ID ของคุณ
       style={{ display: 'block' }}
