e2e0e59348f9f6d8738e7ff4c24590981303d006
imodclub <ikamol.me@gmail.com>
2024-09-05 20:41:14 +0700

    แก้ไขตำแหน่งโฆษณา

--- client/src/Component/MyAd.js
+++ client/src/Component/MyAd.js
@@ -4,10 +4,11 @@ import { Adsense } from '@ctrl/react-adsense';
 const MyAd = () => {
   return (
     <Adsense
-      client='ca-pub-3145740054914889' // แทนที่ด้วย ID ของคุณ
-      slot='5913453722' // แทนที่ด้วย Slot ID ของคุณ
+      client="ca-pub-3145740054914889" // แทนที่ด้วย ID ของคุณ
+      slot="5913453722" // แทนที่ด้วย Slot ID ของคุณ
       style={{ display: 'block' }}
-      format='auto'
+      format="auto"
+      data-overlays="bottom"
     />
   );
 };
