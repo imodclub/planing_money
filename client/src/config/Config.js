458ff543346f09dae70fff1cb0c2f7ec393d7c3c
iModClub <ikamol.me@gmail.com>
2024-09-06 13:50:02 +0700

    แก้ไข apiURL

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,6 +1,6 @@
 const apiURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://planing-money.vercel.app/api'
+    ? 'https://planningmoney.visitors-it.com/api'
     : 'http://localhost:5002/api';
 
 export default apiURL;
