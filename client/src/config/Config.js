42534d9a5fa9ac726ca167c9fff26da0a3643bba
iModClub <ikamol.me@gmail.com>
2024-09-06 15:30:47 +0700

    แก้ apiURL

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,6 +1,6 @@
 const apiURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://planing-money-git-main-imodclubs-projects.vercel.app/'
+    ? 'https://planing-money-git-main-imodclubs-projects.vercel.app/api'
     : 'http://localhost:5002/api';
 
 export default apiURL;
