07ba2ded35b41ae98ef20d4f975d57df172e1125
iModClub <ikamol.me@gmail.com>
2024-09-06 16:04:02 +0700

    อีกจั๊กดอกเบิ่ง

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5002;
 connectDB();
 
 app.use(
-  cors()
+  cors({origin:'https://planningmoney.visitors-it.com',credentials:'true'})
 );
 app.use(express.json());
 app.use(bodyParser.json());
