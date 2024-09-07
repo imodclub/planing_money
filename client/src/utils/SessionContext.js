ed541a629079876845d26ea9e91fcd73a89c6bc0
imodclub <ikamol.me@gmail.com>
2024-09-07 10:08:45 +0700

    เหนื่อยกับการแก้ไข

--- client/src/utils/SessionContext.js
+++ client/src/utils/SessionContext.js
@@ -1,19 +1,23 @@
 import React, { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import apiURL from '../config/Config';
 
 export const SessionContext = createContext();
 
 export const SessionProvider = ({ children }) => {
   const [session, setSession] = useState(null);
+  const [name, setName] = useState(''); // กำหนด state สำหรับ name
+  const [userId, setUserId] = useState(''); // กำหนด state สำหรับ userId
+  const navigate = useNavigate(); // ใช้ useNavigate
 
   useEffect(() => {
     const fetchSession = async () => {
       try {
         const response = await fetch(`${apiURL}/session`, {
-            method: 'GET',
-            headers: {
-                'Contene-Type':'application/json',
-            },
+          method: 'GET',
+          headers: {
+            'Contene-Type': 'application/json',
+          },
           credentials: 'include',
         });
         if (response.ok) {
